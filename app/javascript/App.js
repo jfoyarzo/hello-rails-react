7fe85861582cec41478fdc7dce4adc01d1f6ec42
jfoyarzo <foyarzo621@gmail.com>
2023-03-06 23:44:31 -0300

    Add App and Greetings components

---
+++ app/javascript/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Greetings from './greetings';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Greetings />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,13 +2,16 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import App from "./App";
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
 );
